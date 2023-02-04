9c0a75436026dc905ab58d912aedc38fca90acac
Shish <shish@shishnet.org>
2023-02-04 11:36:53 +0000

    cache based on composer.json

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,14 +1,7 @@
 <?php
-
-$_phpcs_finder = PhpCsFixer\Finder::create()
-  ->in(__DIR__)
-;
-
-$_phpcs_config = new PhpCsFixer\Config();
-return $_phpcs_config->setRules([
+return (new PhpCsFixer\Config())->setRules([
         '@PSR12' => true,
-        //'strict_param' => true,
         'array_syntax' => ['syntax' => 'short'],
     ])
-    ->setFinder($_phpcs_finder)
+    ->setFinder(PhpCsFixer\Finder::create()->in(__DIR__))
 ;
