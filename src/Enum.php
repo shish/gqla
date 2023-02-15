af1ac3b433d932ea122745ededa5fa5a82c1e84f
Shish <shish@shishnet.org>
2023-02-15 21:51:32 +0000

    also add the new file...

---
+++ src/Enum.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GQLA;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Enum
+{
+    public function __construct(
+        public ?string $name=null,
+        public ?string $description=null,
+        public ?string $deprecationReason=null,
+    ) {
+    }
+}
